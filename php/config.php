f3a1426c2b0e93f6372d5303b3b07d7d66757024
brmacedo <53064496+brmacedo@users.noreply.github.com>
2021-02-23 20:06:29 -0300

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "u952250523_Bruno";
-$password = "CJCLNTiW!mtch6cy!Kuj";
-$dbname = "u952250523_alimentador";
+$servername = "SERVER";
+$username = "USERNAME";
+$password = "PASSWORD";
+$dbname = "DABASE_NAME";
 
 
 
@@ -12,10 +12,5 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) die("Connection failed: " . mysqli_connect_error());
 
 
-$chave_encript_produtos = "Au4FJEjq9cQKjXf76KDE9wXmWHD3buqc";
-
 //$conn->close();
-
-
-//Na hora que o alimentador ligar enviar um GET para o ESP32.php nisso ele responde com o MQTT com os horários novos...
 ?>
